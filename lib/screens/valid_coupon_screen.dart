407468e9205bf779357d94bee244de9af7acb331
EzHeisenberg <lucaporal@live.fr>
2021-03-20 17:43:36 +0100

    affiche le qr code

--- lib/screens/valid_coupon_screen.dart
+++ lib/screens/valid_coupon_screen.dart
@@ -62,7 +62,7 @@ class _ValidCouponScreen extends State<ValidCouponScreen> {
                       }
                     }
                 )
-              ],
+              ], s
             )
           )
         ),
